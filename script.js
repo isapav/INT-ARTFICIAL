73513ab30880f9e93dbe469c14bef76d71473f23
isapav <159034243+isapav@users.noreply.github.com>
2024-08-06 14:57:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -73,15 +73,17 @@ function respostaSelecionada(opcaoSelecionada){
         mostraPergunta();
 }
 function jogaNovamente(){
-        atual = 0;
-        historiaFinal = "";
-        mostraPergunta();
+    atual = 0;
+    historiaFinal = "";
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
 }
+
 function mostraResultado() {
-        caixaPerguntas.textContent = "Em 2049...";
-        textoResultado.textContent = historiaFinal;
-        caixaAlternativas.textContent = "";
-        botaoJogarNovamente.addEventListener("click", jogaNovamente());
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent historiaFinal;
+    caixaAlternativas.textContent = "";
+    caixaResultado.classList.add("mostrar");
 }
 
 export const perguntas = [
